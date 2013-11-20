5e4aa070d2e77c6cbe8403d2868bc6fdc120bc70
Yubk <Ybk@YubktekiMacBook-Pro.local>
2013-11-20 13:51:28 +0800

    Initial Commit

---
+++ HouseMagazinePro/MYLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYLAppDelegate.h
+//  HouseMagazinePro
+//
+//  Created by Yubk on 13-11-20.
+//  Copyright (c) 2013年 Yubk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HouseMagazinePro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HouseMagazinePro
+//
+//  Created by Yubk on 13-11-20.
+//  Copyright (c) 2013年 Yubk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MYLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MYLAppDelegate class]));
+    }
+}
